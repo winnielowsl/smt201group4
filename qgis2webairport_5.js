5929842993b6ff47694f501a466edd5754646e01
winnielowsl <91875690+winnielowsl@users.noreply.github.com>
2021-11-13 02:02:13 +0800

    Add files via upload

---
+++ qgis2webairport_5.js
@@ -0,0 +1,1 @@
+var json_qgis2webairport_5 = {"type":"FeatureCollection","name":"qgis2webairport_5","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"REMARK":"Domestic Airport","NAME":"Bandara Sepinggan Balikpapan"},"geometry":{"type":"Point","coordinates":[116.89689,-1.260051]}}]}
